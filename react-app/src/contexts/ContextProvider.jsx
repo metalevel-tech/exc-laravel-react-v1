e2041f934db5d9a8e98afd2ab6df4969a07ce3af
pa4080 <spas.z.spasov@gmail.com>
2023-01-15 17:03:50 +0200

    Contexts - Prepare for acess control: redirect DefaultLayout URIs to /login

---
+++ react-app/src/contexts/ContextProvider.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from "react";
+
+// The default values of the created context are
+// important for the autocompletion function of the IDE.
+const StateContext = createContext({
+  user: null,
+  token: null,
+  setUser: () => { },
+  setToken: () => { }
+});
+
+export const ContextProvider = ({ children }) => {
+  // Create the actual states
+  const [user, setUser] = useState({});
+  const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+
+  const setToken = (token) => {
+    _setToken(token);
+
+    if (token) {
+      localStorage.setItem('ACCESS_TOKEN', token);
+    } else {
+      localStorage.removeItem('ACCESS_TOKEN');
+    }
+  };
+
+  return (
+    // The outer curly braces are for React,
+    // the inner are for the passing JS-object.
+    <StateContext.Provider value={{
+      user,
+      token,
+      setUser,
+      setToken
+    }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
